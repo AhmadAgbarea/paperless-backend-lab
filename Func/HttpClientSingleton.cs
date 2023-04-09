98089bbca497ea47b6f83d6538526c2eb53028c3
Ahmad Agbaria <software.tap.apps@gmail.com>
2023-04-09 07:31:23 +0300

    Add HttpClientSingleton

---
+++ Func/HttpClientSingleton.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Func
+{
+    public static class HttpClientSingleton
+    {
+        public static readonly HttpClient Instance;
+
+        static HttpClientSingleton()
+        {
+            Instance = new HttpClient();
+        }
+    }
+}

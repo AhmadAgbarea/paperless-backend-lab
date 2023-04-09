710827fe2dbed95340bf73c245528f1b98c421e8
Ahmad Agbaria <software.tap.apps@gmail.com>
2023-04-10 01:58:11 +0300

    Add Toolbar

--- WebApp/src/app/app.module.ts
+++ WebApp/src/app/app.module.ts
@@ -4,13 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'; 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import {MatToolbarModule} from '@angular/material/toolbar'; 
+ import {MatIconModule} from '@angular/material/icon'; 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule, FormsModule, BrowserAnimationsModule
+    BrowserModule, HttpClientModule, FormsModule, BrowserAnimationsModule, MatToolbarModule , MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
